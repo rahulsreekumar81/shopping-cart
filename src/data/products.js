cb6c53b8cf67d4ea15f8d95237022454c9756b27
CodeEatSleepRepeat123 <rahulsreekumar81@gmail.com>
2022-03-05 16:44:40 +0530

    first commit

---
+++ src/data/products.js
@@ -0,0 +1,46 @@
+const data = [
+  {
+    id: 1,
+    name: "Ohrensessel Josslyn",
+    price: 3499.99,
+    currency: "INR",
+    image: "images/01.jpg",
+  },
+  {
+    id: 2,
+    name: "Comfy Sofie",
+    price: 5249.99,
+    currency: "INR",
+    image: "images/02.jpg",
+  },
+  {
+    id: 4,
+    name: "Schlafsessel Rovigo",
+    price: 7239.99,
+    currency: "INR",
+    image: "images/04.jpg",
+  },
+  {
+    id: 6,
+    name: "Sessel Little",
+    price: 6119.99,
+    currency: "INR",
+    image: "images/06.jpg",
+  },
+  {
+    id: 5,
+    name: "Sessel Peacock",
+    price: 2599.99,
+    currency: "INR",
+    image: "images/05.jpg",
+  },
+  {
+    id: 6,
+    name: "Sessel Anna",
+    price: 1149.99,
+    currency: "INR",
+    image: "images/03.jpg",
+  },
+];
+
+export default data;
